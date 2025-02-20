f9466e811b500f3c61eb233bb57392f34c9967f2
HoaangQun <hoangquanken97@gmail.com>
2025-02-21 00:58:13 +0700

    2/4

---
+++ 000/DeHSGTinhQN23-24/2. Tep nhat ky/NHATKY.py
@@ -0,0 +1,28 @@
+import re
+
+def cleanlist(la):
+    resu = re.findall(r"-?\d+", la)
+    resu = list(map(int, resu))
+    resu = list(set(resu))
+    return resu
+
+def security_level(la, K):
+    la = cleanlist(la)
+    cp = 0
+    for i in range (len(la)-1):
+        if i+i == K: cp+= 1
+        ssl = la[:i] + la[i+1:]
+        for item in ssl:
+            if la[i] + item == K:
+                cp += 1
+    return cp
+
+with open ("NHATKY.INP", "r") as f:
+    N = int(f.readline())
+    lst = f.readline()
+    K = int(f.readline())
+
+result = security_level(lst, K)
+
+with open ("NHATKY.OUT", "w") as f:
+    f.write(str(result))
